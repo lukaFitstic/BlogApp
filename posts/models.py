ec3fb0353ce1792079a3d1c19609b9677cbdf70f
lukaFitstic <barbieri.luka@fitstic-edu.com>
2024-05-10 17:54:59 +0200

    first commit, set up the django blog app

---
+++ posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    age = models.IntegerField()
+    stageName = models.CharField(max_length=50)
+    def __str__(self):
+        return self.stageName
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author,
+                               on_delete=models.CASCADE,
+                               default = None,
+                               null = True)
+    content = models.TextField()
+    def __str__(self):
+        return self.title
